1a0451d47b132b4ff799bb4f15c97bf6c0f11296
babinskaasia52@gmail.com <babinskaasia52@gmail.com>
2023-01-31 11:05:18 +0100

    class Student change

--- src/main/java/pl/asia/model/Lesson.java
+++ src/main/java/pl/asia/model/Lesson.java
@@ -5,13 +5,88 @@ import java.util.Set;
 
 public class Lesson {
   private String id;
-  private String topic;
-  private LocalDateTime date;
   private Teacher teacher;
   private Set<Student> students;
+  private String topic;
+  private LocalDateTime date;
+  private int duration;
   private int numberOfRoom;
   private int price;
 
+  public Lesson(String id, Teacher teacher, Set<Student> students, String topic, LocalDateTime date, int duration, int numberOfRoom, int price) {
+    this.id = id;
+    this.teacher = teacher;
+    this.students = students;
+    this.topic = topic;
+    this.date = date;
+    this.duration = duration;
+    this.numberOfRoom = numberOfRoom;
+    this.price = price;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public Teacher getTeacher() {
+    return teacher;
+  }
+
+  public void setTeacher(Teacher teacher) {
+    this.teacher = teacher;
+  }
+
+  public Set<Student> getStudents() {
+    return students;
+  }
+
+  public void setStudents(Set<Student> students) {
+    this.students = students;
+  }
+
+  public String getTopic() {
+    return topic;
+  }
+
+  public void setTopic(String topic) {
+    this.topic = topic;
+  }
+
+  public LocalDateTime getDate() {
+    return date;
+  }
+
+  public void setDate(LocalDateTime date) {
+    this.date = date;
+  }
+
+  public int getDuration() {
+    return duration;
+  }
+
+  public void setDuration(int duration) {
+    this.duration = duration;
+  }
+
+  public int getNumberOfRoom() {
+    return numberOfRoom;
+  }
+
+  public void setNumberOfRoom(int numberOfRoom) {
+    this.numberOfRoom = numberOfRoom;
+  }
+
+  public int getPrice() {
+    return price;
+  }
+
+  public void setPrice(int price) {
+    this.price = price;
+  }
 }
 /*
 dodac biblioteke Lombok jako zaleznosc do mavena
