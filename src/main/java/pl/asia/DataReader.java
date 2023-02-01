25ecb00daa13fb81cad33d7e1d9c7181b34d666b
babinskaasia52@gmail.com <babinskaasia52@gmail.com>
2023-02-01 11:43:02 +0100

    Datareader -> addTeacher

--- src/main/java/pl/asia/DataReader.java
+++ src/main/java/pl/asia/DataReader.java
@@ -1,4 +1,45 @@
 package pl.asia;
 
+import pl.asia.io.file.ConsolePrinter;
+import pl.asia.model.Teacher;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 public class DataReader {
+  private Scanner scanner = new Scanner(System.in);
+  private ConsolePrinter consolePrinter;
+
+  public DataReader(ConsolePrinter consolePrinter) {
+    this.consolePrinter = consolePrinter;
+  }
+
+  public Teacher addTeacher() {
+    consolePrinter.printLine("Wprowadź dane nowego nauczyciela");
+    consolePrinter.printLine("Podaj imię");
+    String firstName = scanner.nextLine();
+    consolePrinter.printLine("Podaj nazwisko");
+    String LastName = scanner.nextLine();
+    consolePrinter.printLine("Podaj date urodzenia w formacie YYYY-MM-DD");
+    String dateOfBirth = scanner.nextLine();
+    LocalDate localDateDateOfBirth = LocalDate.parse(dateOfBirth);
+    consolePrinter.printLine("Podaj liczbę przedmiotów, których może uczyć ten nauczyciel");
+    int numberOfSchoolSubject = scanner.nextInt();
+    scanner.nextLine();
+    Set<String> subjectSet = createSubjectSet(numberOfSchoolSubject);
+
+  }
+
+  private Set<String> createSubjectSet(int numberOfSchoolSubject) {
+    Set<String> schoolSubject = new HashSet<>();
+    do {
+      consolePrinter.printLine("Podaj nazwę jednego przedmiotu");
+      String subject = scanner.nextLine();
+      schoolSubject.add(subject);
+    } while (!(schoolSubject.size() == numberOfSchoolSubject));
+    return schoolSubject;
+  }
+
 }
